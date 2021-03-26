9546aeeb3a6153cc221a4def979439791aa85c4a
Pat Rivet <patrickmrivet@gmail.com>
2021-03-26 17:16:35 +0000

    Add styling to Stats component.

--- src/components/Stats/Stats.js
+++ src/components/Stats/Stats.js
@@ -1,10 +1,26 @@
 import { useEffect, useState } from "react";
 import { useSelector } from "react-redux";
+import { makeStyles } from "@material-ui/core/styles";
+import Card from "@material-ui/core/Card";
+import CardContent from "@material-ui/core/CardContent";
+import Typography from "@material-ui/core/Typography";
+import "./Stats.css";
 
 // Custom components
 import { getPostStats } from "../../utils/helperFunctions";
 
 const Stats = () => {
+  const useStyles = makeStyles({
+    root: {
+      width: "30%",
+      margin: 20,
+    },
+    statsValue: {
+      fontWeight: 600,
+    },
+    pos: {},
+  });
+
   const [totalWords, setTotalWords] = useState(0);
   const [mostFrequentWords, setMostFrequentWords] = useState([]);
 
@@ -20,13 +36,39 @@ const Stats = () => {
     }
   }, [posts, loading]);
 
+  const classes = useStyles();
+
   return (
-    <>
-      <div>The word count is {totalWords}</div>
-      <div>
-        The most frequently used words are: {mostFrequentWords.toString()}
-      </div>
-    </>
+    <div className="statsContainer">
+      <Card className={classes.root}>
+        <CardContent>
+          <Typography variant="h5" component="h2">
+            Total Word Count:
+          </Typography>
+          <Typography
+            variant="h5"
+            component="h2"
+            className={classes.statsValue}
+          >
+            {totalWords}
+          </Typography>
+        </CardContent>
+      </Card>
+      <Card className={classes.root}>
+        <CardContent>
+          <Typography variant="h5" component="h2">
+            Top 5 Most Frequent Words:
+          </Typography>
+          <Typography
+            variant="h5"
+            component="h2"
+            className={classes.statsValue}
+          >
+            {mostFrequentWords.toString()}
+          </Typography>
+        </CardContent>
+      </Card>
+    </div>
   );
 };
 
