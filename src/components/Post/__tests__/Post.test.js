bd3fa925cf0e0e96846078d5fb45c5cc8be2e5e8
Pat Rivet <patrickmrivet@gmail.com>
2021-03-29 22:18:05 +0100

    (Refactor): Wrap tests inside describe block.

--- src/components/Post/__tests__/Post.test.js
+++ src/components/Post/__tests__/Post.test.js
@@ -2,23 +2,24 @@ import { render, fireEvent } from "@testing-library/react";
 import configureStore from "redux-mock-store";
 import { Provider } from "react-redux";
 import Post from "../Post";
+describe("Post", () => {
+  const mockStore = configureStore();
+  let store;
+  test("should dispatch an action on button click", () => {
+    store = mockStore({
+      posts: [],
+      loading: false,
+    });
 
-const mockStore = configureStore();
-let store;
-it("should dispatch an action on button click", () => {
-  store = mockStore({
-    posts: [],
-    loading: false,
+    store.dispatch = jest.fn();
+
+    const { getByRole } = render(
+      <Provider store={store}>
+        <Post post={{}} />
+      </Provider>
+    );
+
+    fireEvent.click(getByRole("button"));
+    expect(store.dispatch).toHaveBeenCalledTimes(1);
   });
-
-  store.dispatch = jest.fn();
-
-  const { getByRole } = render(
-    <Provider store={store}>
-      <Post post={{}} />
-    </Provider>
-  );
-
-  fireEvent.click(getByRole("button"));
-  expect(store.dispatch).toHaveBeenCalledTimes(1);
 });
--- src/components/Stats/__tests__/Stats.test.js
+++ src/components/Stats/__tests__/Stats.test.js
@@ -2,20 +2,21 @@ import { render } from "@testing-library/react";
 import configureStore from "redux-mock-store";
 import { Provider } from "react-redux";
 import Stats from "../Stats";
+describe("Stats", () => {
+  const mockStore = configureStore();
+  let store;
+  test("renders correctly", () => {
+    store = mockStore({
+      posts: [],
+      loading: false,
+    });
 
-const mockStore = configureStore();
-let store;
-it("renders correctly", () => {
-  store = mockStore({
-    posts: [],
-    loading: false,
+    const { container } = render(
+      <Provider store={store}>
+        <Stats />
+      </Provider>
+    );
+
+    expect(container).toMatchSnapshot();
   });
-
-  const { container } = render(
-    <Provider store={store}>
-      <Stats />
-    </Provider>
-  );
-
-  expect(container).toMatchSnapshot();
 });
